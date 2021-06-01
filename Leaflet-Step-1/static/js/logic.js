cebf2317627d4e6ab903b34f9585c979c43191a2
Jay Cha <kimchikimchi@gmail.com>
2021-05-31 22:04:55 -0400

    - Added dynamically color coded and sized circle markers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -22,26 +22,56 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?acce
 
 const json_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
+
+const chooseColor = function (num) {
+    // Make sure num is a number;
+    num = +num;
+    let color;
+    switch (true) {
+        case (num >= 90):
+            color = 'red';
+            break;
+        case (num >= 70):
+            color = 'orange';
+            break;
+        case (num >= 50):
+            color = 'lightsalmon';
+            break;
+        case (num >= 30):
+            color = 'yellow';
+            break;
+        case (num >= 10):
+            color = 'lightgreen';
+        default:
+            color = 'green';
+    }
+
+    return color;
+};
+
 /*
  Your data markers should reflect the magnitude of the earthquake by their size and and depth of the earth quake by color. Earthquakes with higher magnitudes should appear larger and earthquakes with greater depth should appear darker in color.
     HINT the depth of the earth can be found as the third coordinate for each earthquake.
     Include popups that provide additional information about the earthquake when a marker is clicked.
     Create a legend that will provide context for your map data.
 */
-
 // Get earthquake data JSON from USGS site.
-d3.json(json_url, quakedata => {
-
+d3.json(json_url, quakeData => {
     // Creating a geoJSON layer with the retrieved data
     // Earthquakes with higher magnitudes should appear larger and earthquakes with greater depth should appear darker in color.
-    L.geoJson(quakedata, feature => { 
-        style: feature => {
-            return {
-                color: "white",      
-                fillColor: "pink",  // dynamically set color based on depth
-                fillOpacity: 0.5,
-                weight: 10.0
-            };
+
+    // See https://leafletjs.com/examples/geojson/ for geoJson option details 
+
+    L.geoJson(quakeData, { 
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius:  feature.properties.mag * 4,        
+                fillColor: chooseColor(feature.geometry.coordinates[2]),
+                color: 'black',
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            });
         }
     }).addTo(myMap);
 });
