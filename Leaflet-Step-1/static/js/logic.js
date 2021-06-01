1c10dfe21f9cfe88225273244d49bf68264616c5
Jay Cha <kimchikimchi@gmail.com>
2021-06-01 00:08:01 -0400

    - Added legend and style

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,22 +2,26 @@
     Create a map using Leaflet that plots all of the earthquakes from your data set based on their longitude and latitude.
 */
 
-// Define a map object
-const myMap = L.map('map', {
-    center: [37, -100],
-    zoom: 5
-});
 
 
 // Create tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+const lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: 'light-v10',
     accessToken: API_KEY
-}).addTo(myMap);
+});
+
+const layerGroup = L.layerGroup();
+
+// Define a map object
+const myMap = L.map('map', {
+    center: [37, -100],
+    zoom: 5,
+    layers: [lightMap, layerGroup]
+});
 
 
 const json_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
@@ -28,22 +32,23 @@ const chooseColor = function (num) {
     num = +num;
     let color;
     switch (true) {
-        case (num >= 90):
+        case num >= 90:
             color = 'red';
             break;
-        case (num >= 70):
+        case num >= 70:
             color = 'orange';
             break;
-        case (num >= 50):
+        case num >= 50:
             color = 'lightsalmon';
             break;
-        case (num >= 30):
+        case num >= 30:
             color = 'yellow';
             break;
-        case (num >= 10):
-            color = 'greenyellow';
-        default:
+        case num >= 10:
             color = 'lightgreen';
+            break;
+        default:
+            color = 'yellowgreen';
     }
 
     return color;
@@ -71,8 +76,25 @@ d3.json(json_url, quakeData => {
         onEachFeature: function(feature, layer) {
             layer.bindPopup(`${feature.properties.title}<hr>Coord: ${feature.geometry.coordinates}`);
         }
-    }).addTo(myMap);
-    
+    }).addTo(layerGroup);
+    // Add the earthquake layer
+    layerGroup.addTo(myMap);
 
     // Create a legend that will provide context for your map data
+    const legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+
+        const depth = [-10, 10, 30, 50, 70, 90];
+        div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+        for (var i =0; i < depth.length; i++) {
+            div.innerHTML += 
+            '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' +
+             depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+
+    legend.addTo(myMap);
 });
